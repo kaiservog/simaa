aec28f185edef0ee814d3b4b06075bea37e7142b
kaiservog <kaiservog@gmail.com>
2017-06-25 22:35:11 -0300

    deixando mais elegante

--- main.go
+++ main.go
@@ -8,8 +8,44 @@ func main() {
   command := os.Args[1]
 
   if command == "deploy" {
+    err := deploy()
+    if err != nil {
+      fmt.Println(err)
+    }
+  } else if command == "hash" {
+    file := os.Args[2]
+    h, err := HashFile(file)
+    if err != nil {
+      fmt.Println("Erro ao gerar Hash", err)
+    }
+    fmt.Println("Hash da aplicação é:", h)
+  } else if command == "teste" {
+    err := testEnvironment()
+    if err != nil {
+      fmt.Println(err)
+    }
+  }
+}
+func testEnvironment() error {
+  err := TestMaven()
+
+  if err != nil {
+    fmt.Println("Erro ao executar o comando 'mvn'", err)
+    return err
+  }
+
+  err = TestSubversion()
+  if err != nil {
+    fmt.Println("Erro ao executar o comando 'svn'", err)
+    return err
+  }
+
+  fmt.Println("Parabéns, Aparentemente tudo esta OK")
+  return nil
+}
+
+func deploy() error {
     branch := os.Args[2]
-    //server := os.Args[3]
 
     CleanWorkDirectory()
 
@@ -18,8 +54,7 @@ func main() {
       err := Checkout(branch, workDir)
       if err != nil {
         fmt.Println("Erro ao baixar branch pelo svn", branch)
-        fmt.Println(err)
-        return
+        return err
       }
     } else {
       _, err := os.Stat(branch)
@@ -32,14 +67,13 @@ func main() {
 
       if err != nil {
         fmt.Println(err)
-        return
+        return err
       }
 
       err = CopyDir(branch, workDir)
       if err != nil {
         fmt.Println("Erro ao copiar código fonte", branch)
-        fmt.Println(err)
-        return
+        return err
       } else {
         fmt.Println("Código fonte copiado com sucesso")
       }
@@ -50,46 +84,15 @@ func main() {
     err := MavenCleanInstall(ConcatPath(mvnPath, "pom.xml"))
     if err != nil {
       fmt.Println("Erro ao realizar mvn clean install em ", mvnPath)
-      fmt.Println(err)
-      return
+      return err
     }
 
-    fmt.Println("Realizando deploy")
+    fmt.Println("Realizando deploy no ATM")
 
     server := SimaaServer{"192.168.1.109", "22", "chip", "chip"}
     deb := GetDebName(mvnPath + "/main/target/")
 
     fmt.Println("Nome do .deb é", deb)
     CopyToServer(server, mvnPath + "/main/target/" + deb, "/tmp/" + deb)
-  } else if command == "remote-deploy" {
-    server := SimaaServer{"192.168.1.109", "22", "chip", "chip"}
-    caminho := "C:\\Users\\cesar\\Documents\\dev\\SIMAA\\biometria-saldo-saque-tev\\caixa-extracash\\main\\target"
-    deb := GetDebName(caminho)
-    fmt.Println("Nome do .deb é", deb)
-    CopyToServer(server, caminho + "/" + deb, "/tmp/" + deb)
-  } else if command == "hash" {
-    file := os.Args[2]
-    fmt.Println(HashFile(file))
-  } else if command == "checkout" {
-    branchPath := os.Args[2]
-    Checkout(branchPath, GetWorkDirectory())
-  } else if command == "work" {
-    CleanWorkDirectory()
-    fmt.Println(GetWorkDirectory())
-  } else if command == "teste" {
-    err := TestMaven()
-
-    if err != nil {
-      fmt.Println("Erro ao executar o comando 'mvn'", err)
-      return
-    }
-
-    err = TestSubversion()
-    if err != nil {
-      fmt.Println("Erro ao executar o comando 'svn'", err)
-      return
-    }
-
-    fmt.Println("Parabéns, Aparentemente tudo esta OK")
-  }
+    return nil
 }
